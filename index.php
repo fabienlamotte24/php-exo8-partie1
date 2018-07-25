888a6f1c886e518ee4ef946b37ab177dc0ff5ebf
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-25 18:04:33 +0200

    exo8p1 php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 8</title>
+  </head>
+  <body>
+      <?php
+        $a = 3 + 4 . '<br />';
+        $b = 5 * 20 . '<br />';
+        $c = 45 / 5;
+        echo $a, $b, $c;
+       ?>
+  </body>
+</html>
