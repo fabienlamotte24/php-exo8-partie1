3dae6f7152372b4b257e447fd2e7ad1928786c0e
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 15:31:46 +0200

    mise en commentaire de l'exercice

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
   </head>
   <body>
       <?php
+      /*Définition des variable avec leur calcul selon la consigne*/
         $a = 3 + 4 . '<br />';
         $b = 5 * 20 . '<br />';
         $c = 45 / 5;
+        /*Affichage des variables respectives*/
         echo $a, $b, $c;
        ?>
   </body>
